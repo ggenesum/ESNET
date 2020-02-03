79f70003308ceee0279b0dcdbaab1a7712251f8a
Lucien Leseigle <lucien.leseigle@epita.fr>
2020-02-03 12:02:44 +0100

    template directory settings

--- source/loginpage.c
+++ source/loginpage.c
@@ -5,6 +5,6 @@ int main(void)
 {
     printf("Content-Type: text/html;\n\n");
     //load_template("headers/styles.html");
-    load_template("templates/loginpage.html");
+    load_template("/ESNET/source/templates/loginpage.html");
     return 0;
 }
